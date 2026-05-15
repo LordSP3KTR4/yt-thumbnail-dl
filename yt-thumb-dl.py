27ec7a81c13e1fccff81ee6c9fab441fc929fdfb
LordSP3KTR4 <danielalejandrocs34@gmail.com>
2026-05-15 14:40:28 -0400

    Reworked so now it allows the input of a complete youtube video url

--- yt-thumb-dl.py
+++ yt-thumb-dl.py
@@ -2,24 +2,45 @@
 
 import requests
 import os
+from urllib.parse import urlsplit
 
-def thumbnailDownloader():
-    videoId = input("Insert youtube video id \n")
+
+
+def obtainVidId(url):
+    splitUrl= urlsplit(url)
+    if splitUrl.netloc == "youtu.be":
+        videoId = splitUrl.path[1:]
+
+    elif splitUrl.netloc == "www.youtube.com":
+        videoId = splitUrl.query[2:]
+
+    else:
+        print("error obtaining id")
+
+    thumbnailDownloader(videoId)
+
+def thumbnailDownloader(videoId):
+    
     thumbnailUrl = "http://img.youtube.com/vi/"+videoId+"/maxresdefault.jpg"
     response = requests.get(thumbnailUrl)
-    fileName = "thumbnails/"+ videoId+".jpg"
     if response.status_code == 200:
-        open(fileName, 'wb').write(response.content)
+        open("thumbnails/"+ videoId+".jpg", 'wb').write(response.content)
         print('Thumbnail downloaded successfully in "thumbnails" folder.')
+
     elif response.status_code == 404:  
         print("Error finding thumbnail, check id and retry.")
+
     else:
         print('Error downloading thumbnail.')
     input("Hit enter to close.")
-    
+
+
+url = input("Insert youtube video url\n")
 try:
-    print("'Thumbnails' folder not found, it will be created now.")
-    os.makedirs("Thumbnails")  
-    thumbnailDownloader()
+    print("'thumbnails' folder not found, it will be created now.")
+    os.makedirs("thumbnails")
+    obtainVidId(url)
+    
 except FileExistsError:
-    thumbnailDownloader()
+    obtainVidId(url)
+
